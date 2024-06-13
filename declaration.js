9fe12577bbfa27be161680b678e6a904269f3bac
junxxx <hprjunxxx@gmail.com>
2024-06-13 11:04:32 +0800

    undefined and null

---
+++ declaration.js
@@ -0,0 +1,12 @@
+// null and undefined
+var init
+
+// undefined
+// if a declarated variable didn't assign value to it
+console.log(init)
+
+var person = null
+// null is a object value
+console.log(person)
+console.log(typeof init)
+console.log(typeof person)
