ed16398f8ecc6537663db010d9cc7730637bf0de
junxxx <hprjunxxx@gmail.com>
2024-06-14 10:13:30 +0800

    declare function hoist

--- function.js
+++ function.js
@@ -1,10 +1,11 @@
 // function declaration
 // it's just a statement
+console.log(incr(1))
+
 function incr(number) {
     return ++number
 }
 
-console.log(incr(1))
 
 // function expression anonymous
 const square = function(num) {
