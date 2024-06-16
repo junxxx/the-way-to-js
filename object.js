40ffac70310787c62e3b3c1da2dce78d23be4766
junxxx <hprjunxxx@gmail.com>
2024-06-16 15:40:59 +0800

    class

--- object.js
+++ object.js
@@ -17,3 +17,39 @@ function Car(make, model, year) {
 }
 const honda = new Car("Honda", "TIL", 1990);
 console.log(honda.model)
+
+class Book {
+    static classTimes = 0;
+
+    constructor(name) {
+        this.name = name
+        Book.classTimes += 1
+    }
+
+    static getClassTimes() {
+        return this.classTimes
+    }
+
+    // instance field
+    times = 0;
+
+    displayTimes = () => { console.log(this.times)}
+
+    // setter
+
+    set times(count) {
+        this.times = count
+    }
+
+}
+
+const js = new Book("JS")
+console.log(Book.getClassTimes())
+const php = new Book("PHP")
+console.log(Book.getClassTimes())
+
+console.log(js.name)
+js.times = 10
+js.displayTimes()
+console.log(php.name)
+php.displayTimes()
